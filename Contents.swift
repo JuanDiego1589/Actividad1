383642cfb00ce78e59ecaaa64aab76e7adbab69a
JuanDiego1589 <juan.d.alvarado15@hotmail.com>
2017-10-15 18:52:33 -0500

    Initial commit

---
+++ Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+for i in 0...100{
+    if(i%5 == 0){
+        print("\(i) Bingo!!!")
+    }
+    if(i%2 == 0){
+        print("\(i) par")
+    }
+    else{
+        print("\(i) impar")
+    }
+    if (i<=40 && i>=30){
+        print("\(i) Viva Swift")
+    }
+}
+
+
+
